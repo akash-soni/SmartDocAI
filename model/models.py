abca9fbbc151692e69c8385bcf6a92b3bf8ed9ea
akash.soni <akash.200287@gmail.com>
2025-07-27 23:03:06 +0530

    logger, exception, document loader & analyzer READY

---
+++ model/models.py
@@ -0,0 +1,14 @@
+# pydantic models are defined here
+from pydantic import BaseModel, Field
+from typing import Optional, List, Any, Union
+
+class Metadata(BaseModel):
+    Summary: List[str]=Field(default_factory=list, description="Summary of the document")
+    Title:str
+    Author:str
+    DateCreated:str
+    LastModifiedDate:str
+    Publisher:str
+    Language:str
+    PageCount: Union[int, str]
+    SentimentTone: str
